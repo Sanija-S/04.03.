962833b41b2c005b161ab15da8f7e4deafa0c43d
Sanija <sanijasokirka@gmail.com>
2024-03-04 09:55:55 +0200

    gtetgetge

--- projekti py/attellugal.py
+++ projekti py/attellugal.py
@@ -1,19 +1,23 @@
 import PySimpleGUI as sg
 import os 
 import io # ievada izvada operacijas, lau teksta straumi parverst pec baitu datiem- nostreamo
-from PIL import image # moulis kkas apstrada attelus
+from PIL import Image # moulis kkas apstrada attelus
 # pip install Pillow
 # python.exe -m pip install --upgrade pip
 
+# Funkcijas, kas ieuto atte;u o failu saraksta no noraditas mapes direktorijas
+def get_image_files(directory):
+    image_files=[f for f in os.listdir(directory) if f.lower().endswith((".png",".jpeg",".jpg", ".gif"))]
+# Funkcija. lai paraditu attelus
+def show_gallery(image_files)
 menu_def=[["File",["Close"]], ["HELP",["About"]]] # izvēļņu josla
 
 buttons=[
-
-   [sg.T("Izvēlies mapi ar attēliem:"), sg.input(key="Input", inable_events=True), sg.FolderBrowse()],
-   [sg.image(key="Image", size=(400,400))],
+   [sg.Menu(menu_def)],
+   [sg.T("Izvēlies mapi ar attēliem:"), sg.Input(key="Input", enable_events=True), sg.FolderBrowse()],
+   [sg.Image(key="Image", size=(400,400))],
    [sg.T("Attēls:"), sg.T("0 / 0", key="Index")],
    [sg.Button("Iziet"), sg.Button("Iepriekš"), sg.Button("Atpakaļ")]
-
 ]
 logs=sg.Window("Attēlu galerija", buttons, finalize=True) # finalizepabeigts nekavejoties pec to izveles, tiek izdaritis visas darbibas pirms fails atverts- izmanto kad darbibas veicamas pirms lodu parada
 while True:
@@ -23,3 +27,15 @@ while True:
     if event=="About":
         sg.popup("Darba autore: Sanija Sokirka")
 logs.close() 
+
+
+
+
+
+
+
+
+
+
+
+#Power Mode
